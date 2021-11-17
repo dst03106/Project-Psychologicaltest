6a3491b47d4e5016e5204e43ea146286f0585e33
dstdbsgml <53514545+dst03106@users.noreply.github.com>
2021-11-17 12:26:40 +0900

    시작 페이지 필수 기능 구현

---
+++ project/src/components/subComponents/TestList.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function TestList(){
+    useEffect
+    return (
+        <>
+            
+        </>
+    );
+}
+
+export default TestList;
